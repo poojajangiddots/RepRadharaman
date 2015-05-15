0bc4c7e8645190f06ea426f5583c9a940b6fef3a
poojajangiddots <pooja.jangid@dotsquares.com>
2015-05-15 18:15:57 +0530

    TestMVCapplication

---
+++ Testwebapp/Testwebapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Testwebapp.MvcApplication" Language="C#" %>
---
+++ Testwebapp/Testwebapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testwebapp.Startup))]
+namespace Testwebapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
